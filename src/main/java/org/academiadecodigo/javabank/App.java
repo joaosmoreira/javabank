b6f38f3952fb37806917f51e626df2f8b17b4455
joaosmoreira <37328807+joaosmoreira@users.noreply.github.com>
2023-03-24 14:39:14 +0000

    alteracoes para spring, criados varios xml, não finalizado

--- src/main/java/org/academiadecodigo/javabank/App.java
+++ src/main/java/org/academiadecodigo/javabank/App.java
@@ -1,22 +1,19 @@
 package org.academiadecodigo.javabank;
 
-import org.academiadecodigo.javabank.controller.Controller;
 import org.academiadecodigo.javabank.persistence.JpaBootstrap;
 import org.academiadecodigo.javabank.persistence.TransactionManager;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaAccountDao;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaCustomerDao;
 import org.academiadecodigo.javabank.persistence.jpa.JpaSessionManager;
 import org.academiadecodigo.javabank.persistence.jpa.JpaTransactionManager;
-import org.academiadecodigo.javabank.services.AccountServiceImpl;
-import org.academiadecodigo.javabank.services.AuthServiceImpl;
-import org.academiadecodigo.javabank.services.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import javax.persistence.EntityManagerFactory;
 
 public class App {
 
     public static void main(String[] args) {
-
+    
         JpaBootstrap jpa = new JpaBootstrap();
         EntityManagerFactory emf = jpa.start();
 
@@ -27,28 +24,31 @@ public class App {
         app.bootStrap(tx, sm);
 
         jpa.stop();
-
+        
     }
 
     private void bootStrap(TransactionManager tx, JpaSessionManager sm) {
+    
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext ();
+        ctx.getEnvironment ().setActiveProfiles (getProfile());
+        // ctx.load(Config)
+        // AccountServiceImpl accountService = new AccountServiceImpl();
+        // accountService.setAccountDao(new JpaAccountDao(sm));
+        // accountService.setTransactionManager(tx);
 
-        AccountServiceImpl accountService = new AccountServiceImpl();
-        accountService.setAccountDao(new JpaAccountDao(sm));
-        accountService.setTransactionManager(tx);
+        // CustomerServiceImpl customerService = new CustomerServiceImpl();
+        // customerService.setCustomerDao(new JpaCustomerDao(sm));
+        // customerService.setTransactionManager(tx);
 
-        CustomerServiceImpl customerService = new CustomerServiceImpl();
-        customerService.setCustomerDao(new JpaCustomerDao(sm));
-        customerService.setTransactionManager(tx);
+        // Bootstrap bootstrap = new Bootstrap();
 
-        Bootstrap bootstrap = new Bootstrap();
+        // bootstrap.setAuthService(new AuthServiceImpl());
+        // bootstrap.setAccountService(accountService);
+        // bootstrap.setCustomerService(customerService);
 
-        bootstrap.setAuthService(new AuthServiceImpl());
-        bootstrap.setAccountService(accountService);
-        bootstrap.setCustomerService(customerService);
-
-        Controller controller = bootstrap.wireObjects();
+        // Controller controller = bootstrap.wireObjects();
 
         // start application
-        controller.init();
+        // controller.init();
     }
 }
